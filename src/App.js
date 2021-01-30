a7a8bb5bef0e85fd6338dd71b4dcdb74df6e69b2
marvio72 <mruvalcaba@oversistemas.com>
2021-01-30 15:53:09 -0600

    Creando un Formulario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 import Header from './componentes/Headers';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <Fragment>
       <Header titulo="Cotizador de Prestamos" descripcion="Ingresa al cotizador y obten tu crédito" />
+      <div className="container">
+        <Formulario />
+      </div>
     </Fragment>
   );
 }
