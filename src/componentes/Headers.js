5041b5a855e7e613cf4b78e78c22995bfd4a4c02
marvio72 <mruvalcaba@oversistemas.com>
2021-01-30 15:24:38 -0600

    Segunda forma de escribir componentes funcionales

--- src/componentes/Headers.js
+++ src/componentes/Headers.js
@@ -1,13 +1,11 @@
 // El nombre de componente siempre debe iniciar con Mayuscula y sin espacios.
 import React, { Fragment } from 'react';
-// Se puede usar props o esto.
-function Header({ titulo, descripcion }) {
-  return (
-    <Fragment>
-      <h1>{titulo}</h1>
-      <p>{descripcion}</p>
-    </Fragment>
-  );
-}
+
+const Header = ({ titulo, descripcion }) => (
+  <Fragment>
+    <h1>{titulo}</h1>
+    <p>{descripcion}</p>
+  </Fragment>
+);
 
 export default Header;
