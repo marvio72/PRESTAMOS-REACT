e7006c7ba5b74b3ffae4fbc62c6cc7e86d19ded0
marvio72 <mruvalcaba@oversistemas.com>
2021-01-30 16:56:27 -0600

    Leer un submit y validar un formulario

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = ({ cantidad, guardarCantidad, plazo, guardarPlazo }) => {
+  // Definir State
+  const [error, guardarError] = useState(false);
+
+  // Cuando el usuario hace submit
+  const calcularPrestamo = (e) => {
+    e.preventDefault();
+
+    // Validar
+    if (cantidad === 0 || plazo === '') {
+      guardarError(true);
+    }
+
+    // Realizar la cotización
+  };
+
   return (
-    <form>
+    <form onSubmit={calcularPrestamo}>
       <div className="row">
         <div>
           <label>Cantidad Prestamo</label>
